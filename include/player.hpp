18598de55aa6134ea5756f57388332d31df36aba
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-20 18:22:44 +0200

    -

---
+++ include/player.hpp
@@ -0,0 +1,16 @@
+/*
+ *
+ * FALP Chess © 2023 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+
+#include "types.hpp"
+
+namespace Chess
+{
+    class player
+    {
+        public:
+        char team;
+    };
+}
