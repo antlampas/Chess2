967384213bc87039c5031372c77ba54e39772d49
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-27 10:40:00 +0200

    -

--- include/types.hpp
+++ include/types.hpp
@@ -1,6 +1,6 @@
 /*
  *
- * FALP Chess © 2023 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ * FALP Chess 2 © 2024 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
  *
  */
 
